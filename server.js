0d209a24266156752ab3ebfd3dc30c9181f9d0f1
KontraStarK <ovkondratiuk19872@gmail.com>
2019-08-14 11:28:01 +0300

    bugfix5

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(express.static(__dirname))
 app.use(bodyParser.urlencoded({
-	extended: false
+	extended: true
 }));
 app.use(bodyParser.json());
 app.use(express.json())
