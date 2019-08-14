abcb72c23a7548618641f224cf053122b4943e26
KontraStarK <ovkondratiuk19872@gmail.com>
2019-08-14 10:40:11 +0300

    bugfix

--- server.js
+++ server.js
@@ -48,5 +48,7 @@ app.put('/hotdogs/edit', function (req, res) {
 		res.send(data);
 	})
 });
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080,function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 console.log("Server run!");
